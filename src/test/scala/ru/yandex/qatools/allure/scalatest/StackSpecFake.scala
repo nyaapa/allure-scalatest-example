c6e88839f388b65cc4af2573545575018c55c9a9
nyaapa <nyaapa@gmail.com>
2015-02-20 04:34:38 +0300

    Added fail

---
+++ src/test/scala/ru/yandex/qatools/allure/scalatest/StackSpecFake.scala
@@ -0,0 +1,14 @@
+package ru.yandex.qatools.allure.scalatest
+
+import org.scalatest._
+import scala.collection.mutable
+import ru.yandex.qatools.allure.annotations.{Features, Title}
+
+@Title("This is a stack fake specification")
+@Features(Array("Simple stack fake"))
+class StackSpecFake extends FlatSpec {
+
+  "Alala" should "aa" in {
+    fail("42")
+  }
+}
